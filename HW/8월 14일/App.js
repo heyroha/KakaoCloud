f9958260ec039c40cf4b550297431c889b8baa49
heyroha <132206033+heyroha@users.noreply.github.com>
2025-08-14 17:55:47 +0900

    Add files via upload

---
+++ HW/8월 14일/App.js
@@ -0,0 +1,13 @@
+/*eslint-disable*/
+
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import MyComponent from './MyComponent';
+import Say from './Say';
+
+const App = () => {
+  return <Say />;
+};
+
+export default App;
---
+++ HW/8월 14일/MyComponent.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+const MyComponent = ({ name, children }) => {
+  return (
+    <div>
+      안녕하세요, 제 이름은 {name} 입니다.
+      <br />
+      children 값은 {children}입니다.
+    </div>
+  );
+};
+
+MyComponent.defaultProps = {
+  name: 'roha',
+};
+
+MyComponent.propTypes = {
+  name: PropTypes.string,
+};
+export default MyComponent;
