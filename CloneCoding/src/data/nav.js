9e8778f7f813843eb6a704ef343daccb50dfa977
heyroha <132206033+heyroha@users.noreply.github.com>
2025-08-19 17:22:38 +0900

    Add files via upload

--- CloneCoding/src/data/nav.js
+++ CloneCoding/src/data/nav.js
@@ -1,1 +1,10 @@
+const nav = [
+  { label: "EXPERIENCE", href: "#!" },
+  { label: "HOTEL", href: "#!" },
+  { label: "DINING", href: "#!" },
+  { label: "CASINO", href: "#!" },
+  { label: "ART-TAINMENT", href: "#!" },
+  { label: "MEETING & WEDDING", href: "#!" },
+];
 
+export default nav;
